8cef2ba6e67d8bb82bbac82bbfe88067af7adebf
Pedro Hernández <44892366+zaintpedro@users.noreply.github.com>
2023-08-09 08:53:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,36 @@
 // Create a web server
 
-// 1. Require express
+// Import the express module
+
 const express = require('express');
 
-// 2. Create an instance of express
+// Create an express application
+
 const app = express();
+
+// Import the body-parser module
+
+const bodyParser = require('body-parser');
+
+// Import the comments module
+
+const comments = require('./comments');
+
+// Configure express to use body-parser as middleware.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Configure express to serve static files from the 'public' folder
+
+app.use(express.static('public'));
+
+// Set the view engine to ejs
+
+app.set('view engine', 'ejs');
+
+// Render the comments page for the path '/comments'
+
+app.get('/comments', (req, res) => {
+    res.render('comments', { comments: comments });
+});
+
