a572fdde35e90fbbb35b6d387acff5e6e3193b71
Pedro Hernández <44892366+zaintpedro@users.noreply.github.com>
2023-08-09 08:50:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -5,26 +5,3 @@ const express = require('express');
 
 // 2. Create an instance of express
 const app = express();
-
-// 3. Define a route handler for the default home page
-//    (GET) /comments
-app.get('/comments', (req, res) => {
-  // 4. Send back a response with the comments
-  res.send([
-    {
-      id: 1,
-      username: 'alice',
-      content: 'first comment',
-    },
-    {
-      id: 2,
-      username: 'bob',
-      content: 'second comment',
-    },
-    {
-      id: 3,
-      username: 'charlie',
-      content: 'third comment',
-    },
-  ]);
-}
